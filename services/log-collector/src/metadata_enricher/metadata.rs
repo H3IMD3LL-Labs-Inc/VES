8da479411930c22f7930b9793304ffaa5ce0f571
officialdennisn <dennisnjuguna278@gmail.com>
2025-09-11 17:36:42 +0300

    Move Log Metadata Configuration to metadata.rs?

---
+++ services/log-collector/src/metadata_enricher/metadata.rs
@@ -0,0 +1,10 @@
+// Metadata {
+//  service: Option<String>,
+//  container: Option<String>,
+//  process: Option<String>,
+//  stream: Option<String>,
+//  flag: Option<String>,
+//  host: Option<String>,
+// }
+//
+// Add metadata to parsed logs
