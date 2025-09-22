d42e33c1875eaa3ffec138b6939604c01d34d00e
d3nn!5 <dennisnjuguna278@gmail.com>
2025-09-23 00:33:14 +0300

    Feature/log collector/buffer batcher v0.0.1 (#11)
    * Moved batcher to buffer source file
    
    * Created new documentation site
    
    * Added toml crate
    
    * Deleted initial documentation website
    
    * Added toml file support for configs
    
    * Made NormalizedLog public for use in buffer initialization in log_buffer_batcher.rs
    
    * Implemented Config backing for log-collector service
    
    * Added rusqlite crate
    
    * Buffer Batcher is now publicly accessible as a module in log-collector
    
    * Basic Buffer Batcher implementation
    
    * Basic configs for log collector
    
    * new() buffer creation
    
    * removed sqlite_path field
    
    * new() creates an empty table after connection is initialized to the database
    
    * fields in NormalizedLog are public
    
    * NormalizedLog can now be pushed to InMemoryBuffer and SQLite persistence layer
    
    * InMemoryBuffer overflow handling
    
    * push() now implements handle_overflow() before adding logs to InMemoryBuffer and SQLite
    
    * Implemented batch_size based batching for flush()
    
    * Implemented time-based flushing in the flush() method implementation for InMemoryBuffer
    
    * push() now calls self.flush() to persist the logs in InMemoryBuffer to SQLite. Logs are flushed on every push() based on user flush_policy config
    
    * Added options comments to guide users on their configuration options
    
    * Implemented hybrid_size_timeout based log flushing
    
    * Updated InMemoryBuffer methods'  documentation
    
    * Implemented drain() to drain logs from InMemoryBuffer after logs are flushed to SQLite storage
    
    * Updated configuration options for drain_policy
    
    * Added notify field to InMemoryBuffer to allow block_with_backpressure overflow handling to pause the parser producer until InMemoryBuffer drains and makes space available to receive more logs.

--- services/log-collector/src/buffer_batcher/log_buffer_batcher.rs
+++ services/log-collector/src/buffer_batcher/log_buffer_batcher.rs
@@ -1,23 +1,3 @@
-// Components
-// 1. Buffer
-// Collects parsed/enriched logs into in-memory Buffer. If durability is set to sqlite,
-// also appends logs into a WAL table for persistence. Basically, acts like a queue ->
-// fast in-memory, optional safety net on disk(production).
-//
-// 2. Batcher
-// Watches Buffers, has 2 flush conditions; Size trigger(number of logs), Time trigger(time since last flush).
-// When triggered; collects a Batch from Buffer (and deletes from WAL if enabled), compresses the batch, returns
-// a compressed batch for use by the shipper.
-//
-// All behavior is defined by log-collector.toml, with the following basic control flow;
-// 1. Log arrives -> Push to in-memory, if durable set also append to WAL
-// 2. Batcher checks conditions -> if enough logs OR time elapsed: create batch
-// 3. Batch creation -> Drain logs from in-memory, if durable set delete correspondence from WAL
-// 4. Batch processing -> Compress with gzip, return compressed batch(to be sent to embedding service via Shipper)
-// 5. Failure handling -> in-memory mode: logs are already gone if process dies/crashes, durable mode: logs still
-// WAL -> retry on restart.
-//
-
 use crate::parser::parser::NormalizedLog;
 use rusqlite::{Connection, Result, params};
 use serde::Deserialize;
@@ -60,7 +40,7 @@ struct InMemoryBuffer {
     overflow_policy: String,
     drain_policy: String,
     flush_policy: String,
-}
+    notify: Arc<Notify>,
 
 /// Configuration-only durability
 #[derive(Debug, Deserialize, Clone)]
@@ -77,7 +57,6 @@ pub enum Durability {
     SQLite(Connection),
 }
 
-/// Methods implementing Buffer
 impl InMemoryBuffer {
     pub async fn load_config(path: &str) -> BufferConfig {
         let buffer_config = std::fs::read_to_string(path).expect("Failed to read config file");
@@ -143,6 +122,7 @@ impl InMemoryBuffer {
             overflow_policy: buffer_config.overflow_policy.clone(),
             drain_policy: buffer_config.drain_policy.clone(),
             flush_policy: buffer_config.flush_policy.clone(),
+            notify: Arc::new(Notify::new()),
         }
     }
 
@@ -158,17 +138,10 @@ impl InMemoryBuffer {
         }
     }
 
-    /// Push NormalizedLogs into an InMemoryBuffer
-    // If buffer is bounded and full -> enforce overflow_policy
-
-    // IMPLEMENTATION
-    // Goal: Combine fast in-memory writes + durable SQLite persistence while minimizing data loss.
-    // 1. In-memory queue: every log is pushed here immediately -> very fast
-    // 2. Async batch flush: periodically take logs from memory -> insert into SQLite -> improve throughput.
-    // 3. Durability safety: prevent data loss if process crashes before batch flush;
-    // - WAL
-    // - Sync flush on shutdown/critical events
-    // - Configurable durability mode (WAL and Sync flush on shutdown/critical events)
+    /// Asynchronously push a `NormlizedLog` to an `InMemoryBuffer`.
+    ///
+    /// Logs in the buffer are persisted to SQLite after each `push_back`
+    /// to the buffer.
     pub async fn push(&mut self, log: NormalizedLog) -> Result<()> {
         if self.buffer_capacity > 0 && self.queue.len() >= self.buffer_capacity as usize {
             self.handle_overflow();
@@ -195,13 +168,13 @@ impl InMemoryBuffer {
         Ok(())
     }
 
-    /// Asynchronously flush batch to SQLite if persistence is enabled,
-    /// while clearing the InMemoryBuffer (handle with care).
+    /// Flush a `NormalizedLog` batch to SQLite if persistence is enabled,
+    /// while clearing the InMemoryBuffer of the flushed logs, to prevent
+    /// unnecessary duplication overhead.
     ///
-    /// This does not clear logs from InMemoryBuffer after they are persisted
-    /// to SQLite
-    ///
-    /// This maintains efficiency, and reduces transaction overhead, improving throughput.
+    /// [Coming Soon]: Flush logs to SQLite persistence without clearing
+    /// InMemoryBuffer, to enable fast lookup by the `Shipper`, without requiring calls
+    /// SQLite persistence.
     pub async fn flush(&mut self, log: NormalizedLog) -> Result<()> {
         match self.flush_policy.as_str() {
             "batch_size" => {
@@ -226,10 +199,9 @@ impl InMemoryBuffer {
                                     ],
                                 )?;
                             }
-                            // TODO: Run `drain()` to clear InMemoryBuffer of already persisted logs
-                            // remember to consider that only the persisted elements should be deleted from InMemoryBuffer
-
                             self.last_flush_at = Instant::now();
+
+                            self.drain();
                         }
                         Durability::InMemory => {
                             eprintln!("InMemoryBuffer durability configured to `in-memory` logs are currently not flushed to SQLite persistent storage");
@@ -262,9 +234,9 @@ impl InMemoryBuffer {
                                     ],
                                 )?;
                             }
-                            // TODO: run `drain()` method to clear InMemoryBuffer of successfully persisted logs
-
                             self.last_flush_at = Instant::now();
+
+                            self.drain();
                         }
                         Durability::InMemory => {
                             eprintln!("InMemoryBuffer durability configured to `in-memory` logs are currently not flushed to SQLite persistent storage");
@@ -275,59 +247,134 @@ impl InMemoryBuffer {
                 Ok(())
             }
             "hybrid_size_timeout" => {
-                // Trigger based on which is best-suited to the current scenario.
-                // Define the actual scenarios and which flush method is best based on these
-                // scenarios you have defined. DO NOT ADD MORE USER CONFIGURATION COMPLEXITY.
+                if self.queue.len() >= self.batch_size {
+                    match &mut self.durability {
+                        Durability::SQLite(conn) => {
+                            let batch_logs: Vec<NormalizedLog> = self.queue
+                                .iter()
+                                .take(self.batch_size)
+                                .cloned()
+                                .collect();
+
+                            for log in batch_logs {
+                                conn.execute(
+                                    "INSERT INTO normalized_logs (timestamp, level, message, metadata, raw_line) VALUES (?1, ?2, ?3, ?4, ?5)",
+                                    params![
+                                        log.timestamp.to_rfc3339(),
+                                        log.level,
+                                        log.message,
+                                        log.metadata.as_ref().map(|m| serde_json::to_string(m).unwrap()),
+                                        log.raw_line
+                                    ],
+                                )?;
+                            }
+                            self.last_flush_at = Instant::now();
+
+                            self.drain();
+                        }
+                        Durability::InMemory => {
+                            eprintln!("InMemoryBuffer durability configured to `in-memory`, logs are currently not being persisted to SQLite");
+                        }
+                    }
+                }
+                if Instant::now().duration_since(self.last_flush_at) > Duration::from_millis(self.batch_timeout_ms) {
+                    match &mut self.durability {
+                        Durability::SQLite(conn) => {
+                            let batch_logs: Vec<NormalizedLog> = self.queue
+                                .iter()
+                                .take(self.queue.len())
+                                .cloned()
+                                .collect();
+
+                            for log in batch_logs {
+                                conn.execute(
+                                    "INSERT INTO normalized_logs (timestamp, level, message, metadata, raw_line) VALUES (?1, ?2, ?3, ?4, ?5)",
+                                    params![
+                                        log.timestamp.to_rfc3339(),
+                                        log.level,
+                                        log.message,
+                                        log.metadata.as_ref().map(|m| serde_json::to_string(m).unwrap()),
+                                        log.raw_line,
+                                    ],
+                                )?;
+                            }
+                            self.last_flush_at = Instant::now();
+
+                            self.drain();
+                        }
+                        Durability::InMemory => {
+                            eprintln!("InMemoryBuffer durability configured to `in-memory`, logs are currently not being persisted to SQLite")
+                        }
+                    }
+                }
 
                 Ok(())
             }
             other => {
-                eprintln!("No flush_policy configured. {} is not a flush_policy option", other);
+                eprintln!("No correct flush_policy configured. {} is not a flush_policy option", other);
                 Ok(())
             }
         }
     }
 
-    /// Asynchronously drain batch, removing logs from in-memory queue after
-    /// successful and confirmed consumption by SQLite.
+    /// Drain a `NormalizedLog` batch removing logs from in-memory queue, freeing up
+    /// space and ensuring an `InMemoryBuffer` remains performant at all times.
     ///
-    /// Basically sends NormalizedLogs in batches to Shipper module.
+    /// `InMemoryBuffer` draining is dependant on configured **drain_policy** and
+    /// **flush_policy**, drain policies are intended to work with certain flush
+    /// policies. Users should configure flush and drain policies while considering
+    /// how they want both to work.
     ///
-    /// Use the set `drain_policy` to determine how to drain the batch
-    pub async fn drain() -> Result<()> {}
+    /// **Use Cases**:
+    /// - `NormalizedLog` batch has been flushed to SQLite persistence **flush()**.
+    pub async fn drain(&mut self) -> Result<()> {
+        match self.drain_policy.as_str() {
+            "drain_all" => {
+                self.queue.drain(..self.queue.len());
+                Ok(())
+            }
+            "drain_batch_size" => {
+                if self.queue.len() >= self.batch_size {
+                    self.queue.drain(..self.batch_size);
+                }
+                Ok(())
+            }
+            "drain_batch_timeout" => {
+                if Instant::now().duration_since(self.last_flush_at) > Duration::from_millis(self.batch_timeout_ms) {
+                    self.queue.drain(..self.queue.len());
+                }
+                Ok(())
+            }
+            other => {
+                eprintln!("No correct drain_policy configured. {} is not a drain_policy option", other);
+                Ok(())
+            }
+        }
+    }
 
-    /// Handle InMemoryBuffer overflow using the overflow_policy
-    /// set in log_collector.toml by user.
+    /// Handle `InMemoryBuffer` overflowing when capacity is approaching
+    /// or exceeding user configured buffer_capacity.
+    ///
+    /// This ensures buffer_capacity is not exceeded while pushing `NormalizedLog`
+    /// to `InMemoryBuffer`. An `InMemoryBuffer` overflowing is handled prior to pushing
+    /// `NormalizedLog` to it.
     async fn handle_overflow(&mut self) -> Result<bool> {
         match self.overflow_policy.as_str() {
             "drop_newest" => {
-                // When buffer is full, reject incoming log.
-                // Preserve all older logs and silently discard/ignore
-                // newest one
                 eprintln!("Buffer full: dropping newest incoming log");
                 Ok(false)
             }
             "drop_oldest" => {
-                // When buffer is full, evict the oldest log to
-                // make room. Always accept the newest log, oldest
-                // history is discarded first
                 self.queue.pop_front();
                 Ok(true)
             }
             "block_with_backpressure" => {
-                // When buffer is full, producer must wait until
-                // there's space. No log is dropped, but log
-                // ingestion slows down.
                 while self.queue.len() >= self.buffer_capacity as usize {
                     self.notify.notified().await;
                 }
                 Ok(false)
             }
             "grow_capacity" => {
-                // When buffer is full, increase its capacity dynamically
-                // No log is ever dropped or blocked, buffer grows as needed.
-                // Prevent producers from oupacing consumers, to prevent OOM
-                // (out-of-memory).
                 self.queue.reserve(1);
                 eprintln!("buffer_capacity exceeded, capacity extended without memory re-allocation")
                 Ok(true)
