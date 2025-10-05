d3bf948745bf5b393734a04d4845f24d55e1b900
officialdennisn <dennisnjuguna278@gmail.com>
2025-10-05 20:33:14 +0300

    Deleted /proto-defs directory

--- services/proto-defs/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_prost_build::compile_protos("proto/collector_embedder.proto")?;
-    Ok(())
-}
-
-// Read tonic-build documentation on github to fine-tune this
