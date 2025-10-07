83b32bb0cd39b9bce1bfca0c27be7788bc8595f5
officialdennisn <dennisnjuguna278@gmail.com>
2025-10-07 14:43:57 +0300

    Updated build.rs to generate client code for log collector micro-service's .proto files

--- services/log-collector/build.rs
+++ services/log-collector/build.rs
@@ -1,7 +1,8 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_client(true)
-        .build_server(true)
+        .build_server(false)
+        .out_dir("src/proto")
         .compile_protos(
             &["../../proto/collector.proto", "../../proto/embedder.proto"],
             &["../../proto"],
