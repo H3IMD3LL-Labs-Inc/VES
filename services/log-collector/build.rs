786aa34b73f387dc6a0e4821b8cf447ce4b8fbf7
officialdennisn <dennisnjuguna278@gmail.com>
2025-10-08 14:35:33 +0300

    collector and embedder server code

--- services/log-collector/build.rs
+++ services/log-collector/build.rs
@@ -1,7 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_client(true)
-        .build_server(false)
+        .build_server(true)
         .out_dir("src/proto")
         .compile_protos(
             &["../../proto/collector.proto", "../../proto/embedder.proto"],
