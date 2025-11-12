90f247865eff4ee63d7626a02fad06a25e568a52
d3nn!5 <dennisnjuguna278@gmail.com>
2025-11-13 01:46:12 +0300

    Improvement/code performance instrumentation (#30)
    * Minimal observability stack
    
    * Fixed syntax error
    
    * Updated Grafana dashboard JSON configuration
    
    * Added hdrhistogram and sysinfo crates for use in code performance instrumentation
    
    * Added hdrhistogram and sysinfo crate for use in code performance instrumentation
    
    * Implement performance instrumentation to track: LOGS_PROCESSED_THIS_SECOND, PROCESS_LINE_DURATION_SECONDS, P99_LATENCY_MS
    
    * Impelement static ref to LOGS_PROCESSED_THIS_SECOND to assist in determining THROUGHPUT_LOGS_PER_SEC
    
    * Implemented STARTUP_DURATION_SECONDS instrumentation, Implemented; Log throughput auto-update background task, p99 latency auto-update background task, memory and cpu usage auto-update background task
    
    * Impelemented SNAPSHOT_RECOVERY_DURATION_SECONDS on new_watcher()

--- services/log-collector/src/helpers/log_processing.rs
+++ services/log-collector/src/helpers/log_processing.rs
@@ -1,6 +1,19 @@
 use crate::buffer_batcher::log_buffer_batcher::InMemoryBuffer;
+use crate::metrics::metrics::{
+    LOGS_PROCESSED_THIS_SECOND, PROCESS_LINE_DURATION_SECONDS, THROUGHPUT_LOGS_PER_SEC,
+    observe_duration,
+};
 use crate::parser::parser::NormalizedLog;
 use crate::shipper::shipper::Shipper;
+
+use hdrhistogram::Histogram;
+use std::sync::Mutex;
+use std::time::Instant;
+
+// Global histogram for latency tracking (accurate to microseconds)
+lazy_static::lazy_static! {
+    pub static ref LAT_HISTOGRAM: Mutex<Histogram<u64>> = Mutex::new(Histogram::new(3).unwrap());
+}
 
 /// Internal helper function to perform actual log processing logic, after logs arrive
 /// (locally or via network).
@@ -11,6 +24,9 @@ pub async fn process_log_line(
     shipper: &Shipper,
     line: String,
 ) -> Result<String, String> {
+    // Start measuring how long processing the log line takes
+    let start = Instant::now();
+
     // Parse the RawLog line into NormalizedLog format
     let parsed_log = match NormalizedLog::select_parser(&line).await {
         Ok(p) => p,
@@ -28,7 +44,7 @@ pub async fn process_log_line(
             >= buffer_batcher.batch_timeout_ms as u128;
 
     // Flush InMemoryBuffer based on user configured flush conditions
-    if should_flush {
+    let result = if should_flush {
         match buffer_batcher.flush(parsed_log.clone()).await {
             Ok(Some(flushed_buffer)) => {
                 if let Err(e) = shipper.send(flushed_buffer.clone()).await {
@@ -42,9 +58,22 @@ pub async fn process_log_line(
             Ok(None) => return Ok("No logs to flush".into()),
             Err(e) => return Err(format!("Flush error: {}", e)),
         }
+    } else {
+        Ok("Log buffered successfully, not flushed.".into())
+    };
+
+    // Record latency into histogram
+    let elapsed_us = start.elapsed().as_micros() as u64;
+    {
+        let mut hist = LAT_HISTOGRAM.lock().unwrap();
+        let _ = hist.record(elapsed_us);
     }
 
-    Ok("Log buffered successfully, not flushed.".into())
-}
+    // Record processing duration
+    observe_duration(PROCESS_LINE_DURATION_SECONDS.clone(), start);
 
-// TODO: Unnecessary clones of parsed_log can be removed to improve performance...
+    // Incremet raw logs processed counter for this second
+    LOGS_PROCESSED_THIS_SECOND.inc();
+
+    result
+}
--- services/log-collector/src/metrics/metrics.rs
+++ services/log-collector/src/metrics/metrics.rs
@@ -12,119 +12,59 @@ pub fn observe_duration(hist: Histogram, start: Instant) {
 }
 
 lazy_static! {
-    // ======== Local Log Ingestion Metrics ========
-    pub static ref LINES_READ_TOTAL: Counter = register_counter!(
-        "logcollector_lines_read_total",
-        "Total number of lines read by tailers"
+    // ======== Core Performance Metrics ========
+
+    /// Sustained throughput (logs/sec/core)
+    pub static ref THROUGHPUT_LOGS_PER_SEC: Gauge = register_gauge!(
+        "logcollector_throughput_logs_per_sec",
+        "Sustained throughput of logs processed per core per second"
     ).unwrap();
 
-    pub static ref FILE_ROTATION_TOTAL: Counter = register_counter!(
-        "logcollector_file_rotation_total",
-        "Number of file rotations handled by tailers"
+    /// Logs processed every second
+    ///
+    /// This static ref is internal, for use to obtain THROUGHPUT_LOGS_PER_SEC
+    pub static ref LOGS_PROCESSED_THIS_SECOND: Counter = register_counter!(
+        "logcollector_logs_processed_this_second_total",
+        "Temporary counter for logs processed within a current 1-second window"
     ).unwrap();
 
-    pub static ref FD_OPEN_TOTAL: Gauge = register_gauge!(
-        "logcollector_fd_open_total",
-        "Number of open file descriptors"
+    /// Average processing latency (derived from process_line_duration histogram)
+    pub static ref PROCESS_LINE_DURATION_SECONDS: Histogram = register_histogram!(
+        "logcollector_process_line_duration_seconds",
+        "Histogram of per-log processing durations in seconds",
+        vec![0.0001, 0.0003, 0.0005, 0.001, 0.002, 0.005]
     ).unwrap();
 
-    // ======== Log Parsing Metrics ========
-    pub static ref PARSE_DURATION_SECONDS: Histogram = register_histogram!(
-        "logcollector_parse_duration_seconds",
-        "Histogram of log parsing durations (seconds)",
-        vec![0.0005, 0.001, 0.002, 0.005, 0.01, 0.02]
-    ).unwrap();
-
-    pub static ref PARSE_FAILURES_TOTAL: Counter = register_counter!(
-        "logcollector_parse_failures_total",
-        "Number of failed log parsing attempts"
-    ).unwrap();
-
-    pub static ref PARSE_BACKPRESSURE_DURATION_SECONDS: Histogram = register_histogram!(
-        "logcollector_parse_backpressure_duration_seconds",
-        "Histogram of parser backpressure durations (seconds)",
-        vec![0.001, 0.002, 0.005, 0.01, 0.02]
-    ).unwrap();
-
-    // ======== InMemoryBuffer Metrics ========
-    pub static ref BUFFER_FLUSH_DURATION_SECONDS: Histogram = register_histogram!(
-        "logcollector_buffer_flush_duration_seconds",
-        "Histogram of InMemoryBuffer flush durations (seconds)",
-        vec![0.001, 0.005, 0.01, 0.02, 0.05]
-    ).unwrap();
-
-    pub static ref BUFFER_FLUSH_ERRORS_TOTAL: Counter = register_counter!(
-        "logcollector_buffer_flush_errors_total",
-        "Total number of InMemoryBuffer flush errors"
-    ).unwrap();
-
-    pub static ref BUFFER_UTITILIZATION_RATIO: Gauge = register_gauge!(
-        "logcollector_buffer_utilization_ratio",
-        "Ratio of InMemoryBuffer utilization(0.0 - 1.0)"
-    ).unwrap();
-
-    pub static ref BUFFER_SIZE: Gauge = register_gauge!(
-        "logcollector_buffer_size",
-        "Current number of logs in the in-memory buffer"
-    ).unwrap();
-
-    // ========= Log Shipper Metrics ========
-    pub static ref SHIP_DURATION_SECONDS: Histogram = register_histogram!(
-        "logcollector_ship_duration_seconds",
-        "Histogram og log shipping durations (seconds)",
-        vec![0.001, 0.005, 0.01, 0.02, 0.05]
-    ).unwrap();
-
-    pub static ref SHIP_QUEUE_DEPTH: Gauge = register_gauge!(
-        "logcollector_ship_queue_depth",
-        "Number of pending batches in shipper queue"
-    ).unwrap();
-
-    pub static ref SHIP_FAILURES_TOTAL: Counter = register_counter!(
-        "logcollector_ship_failures_total",
-        "Total failed ship attempts"
-    ).unwrap();
-
-    // ========= Throughput Metrics ========
-    pub static ref PROCESSED_LOGS_TOTAL: Counter = register_counter!(
-        "logcollector_processed_logs_total",
-        "Total number of successfully processed logs"
-    ).unwrap();
-
-    pub static ref AVERAGE_BATCH_SIZE: Gauge = register_gauge!(
-        "logcollector_average_batch_size",
-        "Average processed logs batch size"
-    ).unwrap();
-
-    // ======== Lifecycle & System Metrics ========
-    pub static ref SHUTDOWN_INVOCATIONS_TOTAL: Counter = register_counter!(
-        "logcollector_shutdown_invocations_total",
-        "Number of graceful shutdown invocations"
-    ).unwrap();
-
-    pub static ref RESTARTS_TOTAL: Counter = register_counter!(
-        "logcollector_restarts_total",
-        "Number of LogCollector restarts"
-    ).unwrap();
-
-    pub static ref DROPPED_LOGS_TOTAL: Counter = register_counter!(
-        "logcollector_dropped_logs_total",
-        "Total number of logs dropped (due to backpressure or errors)"
-    ).unwrap();
-
-    pub static ref ACTIVE_TASKS: Gauge = register_gauge!(
-        "logcollector_active_tasks",
-        "Number of active async tasks in runtime"
-    ).unwrap();
-
-    // ======== High-level Derived Metrics ========
-    pub static ref P99_LATENCY: Gauge = register_gauge!(
+    /// P99 end-to-end latency (milliseconds)
+    pub static ref P99_LATENCY_MS: Gauge = register_gauge!(
         "logcollector_p99_latency_ms",
         "99th percentile end-to-end latency (milliseconds)"
     ).unwrap();
 
-    pub static ref THROUGHPUT: Gauge = register_gauge!(
-        "logcollector_throughput_logs_per_sec",
-        "Number of logs processed per second (smoothed)"
+    // ======== System Resource Metrics ========
+
+    /// Memory footprint in bytes (Grafana converts to MB)
+    pub static ref MEMORY_BYTES: Gauge = register_gauge!(
+        "logcollector_memory_bytes",
+        "Resident memory usage in bytes"
+    ).unwrap();
+
+    /// CPU usage percentage per core
+    pub static ref CPU_PERCENT_PER_CORE: Gauge = register_gauge!(
+        "logcollector_cpu_percent_per_core",
+        "CPU Usage percentage per core"
+    ).unwrap();
+
+    // ======== Lifecycle Metrics ========
+
+    /// Cold start initialization duration in seconds
+    pub static ref STARTUP_DURATION_SECONDS: Gauge = register_gauge!(
+        "logcollector_startup_duration_seconds",
+        "Cold start initialization time (seconds)"
+    ).unwrap();
+
+    pub static ref SNAPSHOT_RECOVERY_DURATION_SECONDS: Gauge = register_gauge!(
+        "logcollector_snapshot_recovery_duration_seconds",
+        "Snapshot recovery duration in (seconds)"
     ).unwrap();
 }
