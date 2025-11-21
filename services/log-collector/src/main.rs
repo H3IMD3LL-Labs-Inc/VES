a8ab92cf92de20777c41531f71cdafe16d46e4d1
officialdennisn <dennisnjuguna278@gmail.com>
2025-11-21 19:08:41 +0300

    Setup tracing method and method initialization in main()

--- services/log-collector/src/main.rs
+++ services/log-collector/src/main.rs
@@ -1,6 +1,7 @@
 mod buffer_batcher;
 mod cli;
 mod helpers;
+mod instrumentation;
 mod metrics;
 mod parser;
 mod proto;
@@ -14,6 +15,9 @@ use anyhow::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    instrumentation::tracing::init_tracing();
+    instrumentation::tracing::init_panic_handler();
+
     // Main entrypoint simply delegates control to CLI layer.
     // The CLI parses user commands and then calls into the appropriate logic
     cli::cli::run().await
