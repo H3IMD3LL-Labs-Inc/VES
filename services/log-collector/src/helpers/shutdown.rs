2dc5a89c81681594f86c751cd1701d464c15bc31
officialdennisn <dennisnjuguna278@gmail.com>
2025-11-24 23:59:50 +0300

    Deleted TODO list

--- services/log-collector/src/helpers/shutdown.rs
+++ services/log-collector/src/helpers/shutdown.rs
@@ -1,30 +1,3 @@
-/// ======================================================================
-///                                 GOALS
-/// ======================================================================
-///
-/// 1. No ongoing work is abruptly interrupted
-/// 2. All pending log data is flushed or persisted
-/// 3. All resources are released safely
-/// 4. The shutdown is co-ordinated across all Log Collector subsystems
-///
-/// ======================================================================
-///                             BUILDING BLOCKS
-/// ======================================================================
-///
-/// 1. Shutdown signal broadcaster
-/// - A central shared object (like Shutdown) that can notify multiple async
-/// tasks at once that it's time to stop.
-///
-/// 2. A signal listener
-/// - A background task that listens for system signals like: CTRL+C(interact
-/// ive stop), SIGTERM(container or systemd stop), SIGINT(manual kill -2).
-/// Once detected, it triggers the broadcaster.
-///
-/// 3. Co-operative shutdown handling in each Log Collector component
-/// - Each long-running component needs to co-operate. They shouldn't just
-/// run infinite loops. They should periodically check for a shutdown event
-/// and exit gracefully when told to. This ensures soft stop rather than
-/// hard termination.
 use tokio::sync::broadcast;
 
 /// Global shutdown manager, built on-top of a broadcast channel
