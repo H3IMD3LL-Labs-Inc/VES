ea2ec27f56c09e61a84c502bebfb78d7786196f1
officialdennisn <dennisnjuguna278@gmail.com>
2026-01-22 18:01:29 +0300

    TailerManager logic

---
+++ components/core-agent/src/tailer/manager.rs
@@ -0,0 +1,63 @@
+// Local crates
+use crate::tailer::{
+    models::TailerManager,
+    tailer_events::{translate_event, handle_event},
+};
+use crate::watcher::models::{WatcherPayload, Checkpoint};
+
+// External crates
+use anyhow::Result;
+use std::collections::HashMap;
+use tokio::sync::broadcast;
+use tokio_util::sync::CancellationToken;
+
+impl TailerManager {
+
+    /// Create a new `TailerManager` once when the pipeline starts for the first
+    /// time or restarts
+    pub fn new(
+      watcher_rx: broadcast::Receiver<WatcherPayload>,
+      shutdown_rx: broadcast::Receiver<()>,
+      checkpoint: Checkpoint,
+      parent_cancel: CancellationToken,
+    ) -> Self {
+        let cancel = parent_cancel.child_token();
+
+        Self {
+            watcher_rx,
+            shutdown_rx,
+            cancel,
+            tailers: HashMap::new(),
+            checkpoint,
+        }
+    }
+
+    /// Continuously receive `WatcherEvent`s from the Watcher and manage the pipeline's
+    /// `Tailer`s based on them. This is the main orchestration loop for all Tailers
+    #[allow(unused_doc_comments)]
+    pub async fn run(mut self) -> Result<()> {
+        loop {
+            tokio::select! {
+                _ = self.cancel.cancelled() => {
+                    break;
+                },
+
+                Ok(_) = self.shutdown_rx.recv() => {
+                    break;
+                },
+
+                Ok(event) = self.watcher_rx.recv() => {
+                    /// [TODO]: Change translate_event() to extract information from
+                    /// WatcherPayload and transform the WatcherEvent to a TailerEvent
+                    let tailer_events = translate_event(event);
+
+                    for event in tailer_events {
+                        handle_event(event).await;
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
