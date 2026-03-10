52acce139022a500471e9f5f4d73985d26cf56bf
officialdennisn <dennisnjuguna278@gmail.com>
2026-03-11 02:13:57 +0300

    Add TxnWriter abstraction providing transactional insert and delete operations using with_rw()

---
+++ components/core-agent/src/recovery/txn_writer.rs
@@ -0,0 +1,59 @@
+// Local crates
+use crate::recovery::{
+    environment::PersistenceEngine,
+    transaction::{
+        TxOutcome,
+        with_rw
+    },
+};
+
+// External crates
+use lmdb::{
+    RwTransaction,
+    Database,
+    WriteFlags,
+    Error as DbError,
+};
+
+pub struct TxnWriter<'a> {
+    env: &'a PersistenceEngine,
+}
+
+impl<'a> TxnWriter<'a> {
+    fn write<F, T>(&self, f: F) -> Result<T, DbError>
+    where
+        F: FnOnce(&mut RwTransaction) -> Result<TxOutcome<T>, DbError>
+    {
+        with_rw(self.env, f)
+    }
+
+    pub fn insert_item<K, V>(
+        &self,
+        db: Database,
+        key: K,
+        value: V,
+    ) -> Result<(), DbError>
+    where
+        K: AsRef<[u8]>,
+        V: AsRef<[u8]>,
+    {
+        self.write(|txn| {
+            txn.put(db, &key, &value, WriteFlags::empty())?;
+            Ok(TxOutcome::Commit(()))
+        })
+    }
+
+    pub fn delete_item<K>(
+        &self,
+        db: Database,
+        key: K,
+    ) -> Result<(), DbError>
+    where
+        K: AsRef<[u8]>,
+    {
+        self.write(|txn| {
+            txn.del(db, &key, None)?;
+            Ok(TxOutcome::Commit(()))
+        })
+    }
+}
