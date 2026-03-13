934834113045aeb0105046f65560e04c00eca77d
officialdennisn <dennisnjuguna278@gmail.com>
2026-03-13 14:15:47 +0300

    Core Agent configuration system directory

---
+++ components/core-agent/src/config/schema/agent.rs
@@ -0,0 +1,99 @@
+// ===================================================================================
+// This is global Core Agent configuration. This configuration affects the
+// runtime environment of a Core Agent itself. The settings allow the Core Agent
+// to affect how its runtime behaves, before sources and processors are affected
+//
+// Think of this as effects on how the Core Agent behaves as a system; work
+// scheduling, data handling, communication with HEIMDELL Server, failures/retries,
+// resource management, etc.
+//
+// This configuration is usually only loaded once on startup, and reloads are
+// usually entire restarts of the whole Core Agent
+// ===================================================================================
+
+use std::time::Duration;
+
+pub enum EnvironmentProvider {
+    Static,
+    Host,
+    Kubernetes { use_namespace: bool },
+    AwsEc2 { use_instance_id: bool },
+    Docker,
+    Custom,
+}
+
+pub enum HEIMDELLServer {
+    // [TODO]: Information about the HEIMDELL Server tied to
+    //         the Core Agent
+}
+
+pub enum NetworkProtocol {
+    Grpc,
+    Http,
+}
+
+pub enum LogLevel {
+    Trace,
+    Error,
+    Warn,
+    Debug,
+    Info,
+}
+
+pub enum Compression {
+    Gzip,
+    Zstd,
+}
+
+pub struct CoreAgentConfig {
+    pub identity: IdentityConfig,
+    pub data_batching: BatchingConfig,
+    pub retries: RetryConfig,
+    pub network: Option<NetworkConfig>,
+    pub runtime: RuntimeConfig,
+    pub data_buffering: BufferConfig,
+    pub telemetry: TelemetryConfig,
+}
+
+pub struct IdentityConfig {
+    pub heimdell_server: Option<HEIMDELLServer>,
+    pub node_id: String,
+    pub hostname: Option<String>,
+    pub environment: Option<String>,
+    pub environment_provider: Option<EnvironmentProvider>,
+}
+
+pub struct BatchingConfig {
+    pub max_events: usize,
+    pub max_batch_bytes: usize,
+    pub flush_interval: Duration,
+}
+
+pub struct RetryConfig {
+    pub max_attempts: u32,
+    pub initial_backoff: Duration,
+    pub max_backoff: Duration,
+}
+
+pub struct NetworkConfig {
+    pub heimdell_server_endpoint: String,
+    pub protocol: NetworkProtocol,
+    pub request_timeout: Duration,
+    pub compression: Compression,
+}
+
+pub struct RuntimeConfig {
+    pub processor_worker_threads: usize,
+    pub max_parallel_sources: usize,
+}
+
+pub struct BufferConfig {
+    pub max_memory_bytes: usize,
+    pub backpressure_threshold: usize,
+}
+
+pub struct TelemetryConfig {
+    pub metrics_enabled: bool,
+    pub metrics_port: Option<u16>,
+    pub log_level: LogLevel,
+}
