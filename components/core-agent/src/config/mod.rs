7afc98f3b59b944b29a178c17ba6fe4c23f55815
officialdennisn <dennisnjuguna278@gmail.com>
2026-03-13 14:15:47 +0300

    Core Agent configuration system directory

---
+++ components/core-agent/src/config/mod.rs
@@ -0,0 +1,5 @@
+pub mod schema;
+pub mod loader;
+pub mod source_providers;
+pub mod source_apis;
+pub mod persistence;
---
+++ components/core-agent/src/config/schema/mod.rs
@@ -0,0 +1,4 @@
+pub mod agent;
+pub mod sources;
+pub mod processor;
+pub mod root;
