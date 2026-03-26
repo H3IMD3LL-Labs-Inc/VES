732396f981be5b53de00edfb6c5faa51544e3832
officialdennisn <dennisnjuguna278@gmail.com>
2026-03-26 23:06:09 +0300

    ConfigManager + process_config(){}

---
+++ components/core-agent/src/config/lifecycle/cfg_manager.rs
@@ -0,0 +1,139 @@
+// This is the high-level configuration lifecycle manager, it uses logic present
+// in /config to actual perform configurations, this same logic is what is used
+// to control the "Continuous Reconciliation Loop" which keeps the Core Agent's
+// configuration in its last persisted desired_state unless a new RootConfig is
+// received which breaks then updates the "Continuous Reconciliation Loop"
+
+#[allow(unused_imports)]
+use crate::config::{
+    schema::root::{RootConfig, ConfigProvider},
+    lifecycle::{
+        validation::validate_root_config,
+        version::ConfigVersion,
+        diff::{diff_root, RootConfigDiff},
+        state::LiveState,
+        apply::apply_config,
+        cfg_events::{
+            ConfigEvent,
+            ConfigValidation,
+            ApplyStatus
+        },
+    }
+};
+
+use std::time::SystemTime;
+
+pub struct ConfigManager {
+    pub state: LiveState,
+    pub last_applied_version: Option<ConfigVersion>,
+}
+
+impl ConfigManager {
+    pub fn new(state: LiveState) -> Self {
+        Self {
+            state,
+            last_applied_version: None,
+        }
+    }
+
+    pub fn process_config(
+        &mut self,
+        source: ConfigProvider,
+        config: RootConfig,
+        raw_bytes: &[u8],
+    ) -> Vec<ConfigEvent> {
+        let mut events = Vec::new();
+
+        let version = ConfigVersion::from_root_config_bytes(raw_bytes);
+
+        if self.last_applied_version.as_ref() == Some(&version) {
+            return events;
+        }
+
+        events.push(ConfigEvent::NewSnapshot {
+            source: source.clone(),
+            version: version.clone(),
+            received_at: SystemTime::now(),
+        });
+
+        let validation = validate_root_config(&config);
+
+        if !validation.is_valid() {
+            for err in validation.errors {
+                events.push(ConfigEvent::Validation {
+                    source: source.clone(),
+                    version: version.clone(),
+                    result: ConfigValidation::Failed { errors: err },
+                });
+            }
+
+            return events;
+        }
+
+        for warn in validation.warnings {
+            events.push(ConfigEvent::Validation {
+                source: source.clone(),
+                version: version.clone(),
+                result: ConfigValidation::Success { warnings: warn },
+            });
+        }
+
+        let diff = match &self.state.last_applied_config {
+            Some(old) => diff_root(old, &config),
+            None => diff_empty_root_config(&config),
+        };
+
+        if !diff.has_changes {
+            events.push(ConfigEvent::NoChanges {
+                source,
+                version,
+                diffs: diff,
+            });
+
+            return events;
+        }
+
+        events.push(ConfigEvent::DiffPresent {
+            source: source.clone(),
+            version: version.clone(),
+            diff: diff.clone(),
+        });
+
+        events.push(ConfigEvent::SnapshotApplied {
+            source: source.clone(),
+            version: version.clone(),
+            status: ApplyStatus::Started,
+        });
+
+        match apply_config(&mut self.state, diff, config.clone()) {
+            Ok(_) => {
+                self.last_applied_version = Some(version.clone());
+
+                events.push(ConfigEvent::SnapshotApplied {
+                    source: source.clone(),
+                    version: version.clone(),
+                    status: ApplyStatus::Succeeded,
+                });
+            }
+            Err(e) => {
+                events.push(ConfigEvent::SnapshotApplied {
+                    source,
+                    version,
+                    status: ApplyStatus::Failed { error: e },
+                });
+
+                return events;
+            }
+        }
+
+        // [TODO]: Perform actual persistence of the successfully applied
+        //         RootConfig as the "latest" state/config
+        events.push(ConfigEvent::PersistedSnapshot { version });
+
+        events
+    }
+}
+
+fn diff_empty_root_config(new: &RootConfig) -> RootConfigDiff {
+    diff_root(&RootConfig::empty(), new)
+}
