7902597fa2f7790241dcab33ab74eba5ea0f471f
officialdennisn <dennisnjuguna278@gmail.com>
2026-04-02 12:56:29 +0300

    Buf configurations and Buf codegen configurations

--- components/ves_lib/src/lib.rs
+++ components/ves_lib/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#[path = "codegen/buffa/mod.rs"]
+pub mod proto;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#[path = "codegen/connect/mod.rs"]
+pub mod connect;
